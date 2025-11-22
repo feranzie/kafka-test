405d4ccc7f8ab918204f4a487d12c79ee884b593
feranzie <oladaviesferanmi@gmail.com>
2025-11-22 05:49:44 -0800

    producer fix

--- producer.py
+++ producer.py
@@ -1,4 +1,4 @@
-from connfluent_kafka import Producer
+from confluent_kafka import Producer
 import json
 import uuid
 #boot strap refers to kafka using the adres to discover all brokers, odes, servers at the address
@@ -16,11 +16,11 @@ def delivery_report(err, msg):
         print(f"✅ Delivered {msg.value().decode("utf-8")}")
         print(f"✅ Delivered to {msg.topic()} : partition {msg.partition()} : at offset {msg.offset()}")
 
-order =(
+order ={
     "order_id" : str(uuid.uuid4()),
     "user":"nana",
     "item" : "pizza",
-    "quantity": 5 )
+    "quantity": 5 }
 
 #turn json to str then bytes
 value=json.dumps(order).encode('utf-8')
@@ -28,7 +28,7 @@ value=json.dumps(order).encode('utf-8')
 #creates topic called orders and sends the value
 producer.produce(
     topic='orders', 
-    value=value
+    value=value,
     ##track whether message was successfully delivered or not
     callback=delivery_report
     )
